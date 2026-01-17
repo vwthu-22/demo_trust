58f876a66c3fa3e720810ffff26477d00a6443cf
VanThu-09 <nguyenvanthu16092004@gmail.com>
2026-01-17 14:23:49 +0700

    update

--- fe/app/page.tsx
+++ fe/app/page.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/Header';
 import Banner from '@/components/Banner';
 import Products from '@/components/Products';
 import Footer from '@/components/Footer';
+import Reviews from '@/components/review';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <Banner />
       <Products />
+      <Reviews />
       <Footer />
     </main>
   );
